159d9b315f72eb15dbd05633c1173bf1cc421b85
Manu <Manu@Manu-PC>
2015-02-25 04:26:39 +0300

    updated form with markup entry plus added a speech tagger

---
+++ src/sphinx4/encognn/hybrid/util/speechtagger.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sphinx4.encognn.hybrid.util;
+
+/**
+ *
+ * @author Manu
+ */
+public class speechtagger {
+    
+}
